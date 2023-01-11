7264d53ebf0226c6c7f9e9455b9015ba5d5d4894
flypig <testad@qq.com>
2023-01-11 23:22:43 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const isCheckUrl = (url) => url.includes(CHECK_URL)
 const getbalance = (url) => url.includes('myhongbao_getHongBaoBalance')
 const getusableHonglist = (url) => url.includes('getUsableHongBaoList')
 
-if (isCheckUrl(url) && $response.status == 200) {
+if (isCheckUrl(url) && $response.status == 404) {
   const unlock = {"balanceMap":{"totalUsableBalance":17.33,"allLimitOrgBalance":0,"totalPreBalance":0,"totalCurrentBalance":17.33,"allOrgBalance":17.33,"totalDisableBalance":0},"count":1,"message":"查询成功！祝您购物愉快~","resultCode":200,"success":true,"tid":0,"totalBalance":17.33}
 
   const status = 200
